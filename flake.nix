8c86e8c485ace077fd4af53e5d1cca3d2c0dcbd0
sandbushman <NatohBbink@tutamail.com>
2026-03-21 23:56:21 +0800

    now switched to a flake configuration. here's hoping it's still functional after i restart

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    nix4vscode = {
+      url = "github:nix-community/nix4vscode";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { self, nixpkgs, nixpkgs-unstable, nixos-hardware, nix4vscode, ... }@inputs:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          {
+            nixpkgs.overlays = [ nix4vscode.overlays.default ];
+          }
+          ./configuration.nix
+        ];
+      };
+    };
+}
